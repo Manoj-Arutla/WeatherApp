ab89fbab1be44a3607f918c5d5bd86a4f187f410
Manoj-Arutla <manojarutla9@gmail.com>
2026-02-10 15:57:20 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const API_KEY = "bd5e378503939ddaee76f12ad7a97608";
+
+const cityInput = document.getElementById("cityInput");
+const searchBtn = document.getElementById("searchBtn");
+const cityEl = document.querySelector('#city');
+const tempEl = document.querySelector('#temp');
+const conditionEl = document.querySelector('#condition');
+const feelsEl = document.querySelector('#feels');
+const humidityEl = document.querySelector('#humidity');
+const windEl = document.querySelector('#wind');
+
+
+searchBtn.addEventListener("click", fetchWeather);
+cityInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") fetchWeather();
+});
+
+async function fetchWeather() {
+  const city = cityInput.value.trim();
+
+  if (!city) {
+    alert("Please enter a city name");
+    return;
+  }
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error("City not found");
+    }
+
+    const data = await response.json();
+
+    cityEl.textContent = data.name;
+    tempEl.textContent = `${Math.round(data.main.temp)}°C`;
+    conditionEl.textContent = data.weather[0].main;
+    feelsEl.textContent = `Feels like ${Math.round(data.main.feels_like)}°C`;
+    humidityEl.textContent = `Humidity: ${data.main.humidity}%`;
+    windEl.textContent = `Wind: ${data.wind.speed} m/s`;
+
+
+
+    
+
+  } catch (error) {
+    alert(error.message);
+  }
+}
